bcbcf7e04dc98994eb23248a9c8b44f613c5bbcf
Yoav Miller <yoav.miller@utilight.com>
2026-02-24 00:16:26 +0200

    try to fix Google login

--- frontend/src/app/core/interceptors/auth.interceptor.ts
+++ frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -1,17 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { environment } from '../../../../environments/environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const auth = inject(AuthService);
   const token = auth.getToken();
 
-  if (token) {
-    const cloned = req.clone({
-      setHeaders: { Authorization: `Bearer ${token}` }
-    });
-    return next(cloned);
-  }
+  const url = req.url.startsWith('/') && environment.apiUrl
+    ? environment.apiUrl + req.url
+    : req.url;
 
-  return next(req);
+  const cloned = req.clone({
+    url,
+    ...(token ? { setHeaders: { Authorization: `Bearer ${token}` } } : {})
+  });
+
+  return next(cloned);
 };
