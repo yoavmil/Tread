22c2c0d174083457880c3887d27404ced4230953
Yoav Miller <yoav.miller@utilight.com>
2026-02-23 23:45:28 +0200

    trust proxy

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ const submissionsRoutes = require('./routes/submissions');
 
 const app = express();
 
+app.set('trust proxy', 1);
 app.use(helmet());
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:4200',
