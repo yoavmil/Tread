b9a543678fbadb7a6c165ff08e674b10c50fe802
Yoav Miller <yoav.miller@utilight.com>
2026-02-24 00:20:54 +0200

    fix path

--- frontend/src/app/core/interceptors/auth.interceptor.ts
+++ frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const auth = inject(AuthService);
