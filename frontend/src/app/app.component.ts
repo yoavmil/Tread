0cc4b38df740e3044b4c1ed830eab187f9190faa
Yoav Miller <yoav.miller@utilight.com>
2026-03-11 14:18:30 +0200

    load user on init

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `<router-outlet />`
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private auth: AuthService) {}
+
+  ngOnInit(): void {
+    if (this.auth.getToken()) {
+      this.auth.loadUser().subscribe({
+        error: () => this.auth.clearSession()
+      });
+    }
+  }
+}
